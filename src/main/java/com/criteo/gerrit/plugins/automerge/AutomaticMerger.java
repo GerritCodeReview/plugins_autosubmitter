3f25ccf9d2c7a17e76008da111e7ee79b8d258d1
Matthias Sohn <matthias.sohn@sap.com>
2023-01-30 09:54:26 +0100

    Adapt to API changes in Gerrit 3.7 introduced in I3fccb05d21
    Release-Notes: Adapt to API changes in Gerrit 3.7
    Change-Id: Id92a98959a801914ebc7d4b54cff753dedf90c20

--- src/main/java/com/criteo/gerrit/plugins/automerge/AutomaticMerger.java
+++ src/main/java/com/criteo/gerrit/plugins/automerge/AutomaticMerger.java
@@ -32,7 +32,7 @@ import com.google.gerrit.server.events.PatchSetCreatedEvent;
 import com.google.gerrit.server.events.RefUpdatedEvent;
 import com.google.gerrit.server.events.ReviewerDeletedEvent;
 import com.google.gerrit.server.events.TopicChangedEvent;
-import com.google.gerrit.server.git.MergeUtil;
+import com.google.gerrit.server.git.MergeUtilFactory;
 import com.google.gerrit.server.query.change.ChangeData;
 import com.google.gerrit.server.restapi.change.GetRelated;
 import com.google.gerrit.server.restapi.change.PostReview;
@@ -59,7 +59,7 @@ public class AutomaticMerger implements EventListener, LifecycleListener {
 
   @Inject GetRelated getRelated;
 
-  @Inject MergeUtil.Factory mergeUtilFactory;
+  @Inject MergeUtilFactory mergeUtilFactory;
 
   @Inject Provider<PostReview> reviewer;
 
